0ddb9edbbc82c0dd67244735f5bf4fd54996d415
David Abram <david@abram.com>
2018-05-26 11:22:01 +0200

    button

--- stories/button.js
+++ stories/button.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 /* eslint-enable */
-import { Button } from '../dist/index';
+import Button from '../components/button';
 
 const buttonStory = storiesOf('Button', module);
 
