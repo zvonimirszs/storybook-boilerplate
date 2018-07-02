fdc0cd4da2e575526fa30f85d3b54a60b1b47924
zvonimirs <zvonimirs@MANAGEMENTCRO3>
2018-07-02 17:03:32 +0200

    1

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 export Button from './button';
 export Card from './card';
 export Header from './header';
-export Image from './image';
+export Image from './image-text';
 export Footer from './footer';
 export Menu from './menu';
